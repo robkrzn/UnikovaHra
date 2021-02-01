fa8234f3763795782652227f9bd31e4920230f37
robkrzn <robert1krizan2@gmail.com>
2021-02-02 00:11:59 +0100

    zaciatok morseovky

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include <U8x8lib.h>
 
 #define MAXMOZNOSTI 5                                                                                             //max moznosti hier ktoru su k dispozicii
-const String nazvyHier[MAXMOZNOSTI] = {"Svetelna brana", "TEST HRY 2", "TEST HRY 3", "TEST HRY 4", "TEST HRY 5"}; //nazvy hier z menu
+const String nazvyHier[MAXMOZNOSTI] = {"Svetelna brana", "Morseovka", "TEST HRY 3", "TEST HRY 4", "TEST HRY 5"}; //nazvy hier z menu
 
 U8X8_SH1106_128X64_NONAME_HW_I2C u8x8(/* reset=*/U8X8_PIN_NONE); //displej definicia
 
@@ -12,7 +12,7 @@ const int TlacidloCervene = 19; //pintlacidla
 const int PhotoresistorPin = 2; //pin footorezistora
 
 //globalne pomocne premenne
-int i = 1, y = 0, x = 1; //pomocne premenne
+int y = 0, x = 1; //pomocne premenne
 bool zapnutaHra = false;
 int StavModrehoTlacidla; //nove pomocne tlacidlo
 int hodnotaPhotorezistora;
@@ -33,6 +33,45 @@ void svetelnaBrana()
 
   u8x8.setFont(u8x8_font_inb33_3x6_n);
   u8x8.drawString(0, 2, u8x8_u16toa(hodnotaPhotorezistora, 4));
+}
+
+void morseovka(){
+  int pocitadloZnaku=0;
+  int poccitadloMedzery=0;
+  char znak[5]={0};
+  while(true){
+  hodnotaPhotorezistora = analogRead(PhotoresistorPin);
+
+    while(hodnotaPhotorezistora<600 && pocitadloZnaku <10000){
+      pocitadloZnaku++;
+      hodnotaPhotorezistora = analogRead(PhotoresistorPin);
+      //poccitadloMedzery=0;
+    }
+    /*
+    while(hodnotaPhotorezistora>=600 && poccitadloMedzery <20000){
+      poccitadloMedzery++;
+      hodnotaPhotorezistora = analogRead(PhotoresistorPin);
+    }
+    */
+    if(digitalRead(TlacidloCervene)==true){
+      pocitadloZnaku=0;
+      u8x8.setFont(u8x8_font_amstrad_cpc_extended_f);
+      u8x8.drawString(0, 2, u8x8_u16toa(pocitadloZnaku, 4));
+      u8x8.setCursor(5, 2);
+      u8x8.print("null");
+    }
+    if(pocitadloZnaku>0){
+    u8x8.setFont(u8x8_font_amstrad_cpc_extended_f);
+    u8x8.drawString(0, 2, u8x8_u16toa(pocitadloZnaku, 4));
+    }
+    /*
+    if(poccitadloMedzery>0){
+      u8x8.setFont(u8x8_font_amstrad_cpc_extended_f);
+      u8x8.drawString(0, 3, u8x8_u16toa(poccitadloMedzery, 4));
+    }
+    */
+  }
+
 }
 
 void loop()
@@ -67,4 +106,6 @@ void loop()
   //MENU
   if (zapnutaHra == true && y == 0)
     svetelnaBrana();
+  if(zapnutaHra == true && y == 1)
+    morseovka();
 }
